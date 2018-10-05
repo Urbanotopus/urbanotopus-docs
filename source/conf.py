2588e440ccfd00bb9d93df31451b0b5db7663f6e
NyanKiyoshi <hello@vanille.bid>
2018-10-05 21:15:15 +0200

    Add Python 3.5 to 3.8 and Mac OS to travis builds

--- source/conf.py
+++ source/conf.py
@@ -132,7 +132,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'urbanotopus.tex', 'urbanotopus Documentation',
-     'KOCAK Mikail, GROSJEAN Martin, RAZAFINDRATSITA Loïc', 'manual'),
+     author, 'manual'),
 ]
 
 
